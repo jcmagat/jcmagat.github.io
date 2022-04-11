f5624cf77894de1af26520aa291eeea28e7b8fe5
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-04-11 13:45:15 -0700

    Add script to toggle open navigation
    Also improve styling of navigation

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const navButton = document.querySelector(".nav-button");
+const navLinks = document.querySelectorAll(".nav-link");
+
+navButton.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
