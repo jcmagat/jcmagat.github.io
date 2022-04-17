37e4295dd7c1d516cffdc1f6bf7b8da4d55799e4
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-04-17 13:12:39 -0700

    Improve contact input invalid warning

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+/* ========== Navigation Toggle ========== */
+
 const navButton = document.querySelector(".nav-button");
 const navLinks = document.querySelectorAll(".nav-link");
 
@@ -8,5 +10,23 @@ navButton.addEventListener("click", () => {
 navLinks.forEach((link) => {
   link.addEventListener("click", () => {
     document.body.classList.remove("nav-open");
+  });
+});
+
+/* ========== Contact Input Validation ========== */
+
+const contactInputs = document.querySelectorAll(".contact-input");
+
+contactInputs.forEach((input) => {
+  input.addEventListener("invalid", (event) => {
+    event.preventDefault();
+
+    input.parentElement.classList.add("contact-input-invalid");
+  });
+});
+
+contactInputs.forEach((input) => {
+  input.addEventListener("focus", () => {
+    input.parentElement.classList.remove("contact-input-invalid");
   });
 });
